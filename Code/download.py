10250493de714fdcf1d2a20e91892281b70b0a4f
Bdbmzwsc <88311279+Bdbmzwsc@users.noreply.github.com>
2022-09-24 19:34:04 +0800

    added download

---
+++ Code/download.py
@@ -0,0 +1,19 @@
+import os
+from sys import platform
+import requests
+import aiohttp
+import asyncio
+import time
+
+class Download:
+    def __init__(url,path):
+        self.url=url
+        self.path=path
+
+    async def fetch(session)
+
+
+
+
+if __name__=='__main__':
+    print('helloworld')
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # coding=utf-8
 """主文件"""
-from .Code.MainWindow import Run
+#from .Code.MainWindow import Run
+import sys
+import os
+sys.path.append('/')
+from Code.MainWindow import Run
 
 if __name__ == '__main__':
     Run()
