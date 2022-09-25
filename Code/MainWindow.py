42ce4f30999bd1613f4acb3be5fde311aa2978f1
xianyongjiian080402 <xianyongjian080402@163.com>
2022-09-25 12:01:37 +0800

    📝 :优化页面 - 点击切换动画10% - 加载页面动图100% #2

--- Code/MainWindow.py
+++ Code/MainWindow.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 import os.path
 
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtGui
 from PyQt6.QtWidgets import QMainWindow, QGraphicsOpacityEffect
-from PyQt6.QtCore import QTimer, QThread, Qt
+from PyQt6.QtCore import QTimer, QThread, Qt, QRect, QPropertyAnimation
+from PyQt6.QtGui import QPainter, QPen, QColor, QBrush
 import sys
 from UI.MainWindow.MainWindow import Ui_MainWindow
 
@@ -11,6 +12,7 @@ from UI.MainWindow.MainWindow import Ui_MainWindow
 class RunUi(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(RunUi, self).__init__()
+        import UI.MainWindow.img_rc
         self.setupUi(self)
         self.show()
         print("已成功显示窗体")
@@ -22,10 +24,18 @@ class RunUi(QMainWindow, Ui_MainWindow):
 
         self.pushButton_hello_start.clicked.connect(self.FirstStartInitialize)
 
+
+
     def RunInitialize(self, First=True):
         """在启动器启动后初始化启动器(读取设置+设置启动器)"""
         if First == True:
             self.RunInitialize_.stop()
+
+        # 开始播放动图
+        self.gif = QtGui.QMovie(":/widget_Sidebar/images/MOS_Logo_gif.gif")
+        self.label_loading_gif.setMovie(self.gif)
+        self.gif.start()
+
         self.JsonFile = os.path.join('')
         from Code.Code import JsonRead, JsonFile, InitializeFirst
         F = JsonFile()
@@ -38,6 +48,7 @@ class RunUi(QMainWindow, Ui_MainWindow):
             print('Json读取完成')
             self.label_loading_text_2.setText('正在设置启动器(2/2)')
             self.Animation_ToMainWindow()
+            self.gif.stop()
 
     def FirstStartInitialize(self):
         """在第一次启动时 初始化(缓存)"""
@@ -45,17 +56,23 @@ class RunUi(QMainWindow, Ui_MainWindow):
         InitializeFirst()
         self.Animation_ToMainWindow(HelloToMainLoading=True)
 
+
+
     def FirstStartInitializeOk(self):
         """在第一次启动时 初始化(缓存) 的页面切换动画完成后"""
         self.RunInitialize(First=False)  # 重新读取配置
 
     def Animation_ToMainWindow(self, HelloToMainLoading=False):
-        """动画函数-……(默认 加载)页面->主页面"""
+        """
+            动画函数-……(默认 加载)页面->主页面
+
+            HelloToMainLoading: 从欢迎页面到加载页面
+        """
 
         # 切换为第……页
         if HelloToMainLoading == False:
             self.Animation_ToMainWindow_Int_Page = 0
-        elif HelloToMainLoading == True:
+        elif HelloToMainLoading:
             # 欢迎页切换为加载页
             self.Animation_ToMainWindow_Int_Page = 1
 
@@ -101,7 +118,7 @@ class RunUi(QMainWindow, Ui_MainWindow):
 
         # 触发切换动画(淡出)
         self.Animation_ToMainWindow_Run = QTimer()
-        self.Animation_ToMainWindow_Run.start(2)
+        self.Animation_ToMainWindow_Run.start(1)
         self.Animation_ToMainWindow_Run.timeout.connect(Animation)
 
 
