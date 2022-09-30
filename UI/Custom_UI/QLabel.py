38398faaa7a94cd1e4d2e7ad182d5552362a5e32
xianyongjiian080402 <xianyongjian080402@163.com>
2022-09-30 17:28:19 +0800

    📝 :优化页面 - 点击切换动画10% - 加载页面动图100% #2

---
+++ UI/Custom_UI/QLabel.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+from PyQt6 import QtWidgets, QtCore, QtGui
+
+
+class MyQLabel(QtWidgets.QLabel):
+    # 自定义单击信号
+    clicked = QtCore.pyqtSignal()
+
+    def __int__(self):
+        super().__init__()
+
+    # 重写单击事件
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.clicked.emit()
