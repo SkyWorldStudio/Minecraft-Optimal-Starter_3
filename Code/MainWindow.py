f35321ce3d241a1f2d0a0c498d8476b99b052336
HeimNad <107762511+HeimNad@users.noreply.github.com>
2023-01-06 07:57:37 +0800

    修复了windows启动失败问题

--- Code/MainWindow.py
+++ Code/MainWindow.py
@@ -1249,7 +1249,7 @@ class RunUi(QMainWindow, Ui_MainWindow):
                 import platform
                 M_T = platform.platform()
                 Systeam_V = str(M_T).split('-')[1]
-                Places = int(str(platform.architecture()[1]).split('bit')[0])
+                Places = int(str(platform.architecture()[0]).split('bit')[0])
                 self.Json_MOS['Systeam_Places'] = Places
                 self.Json_MOS['Systeam_V'] = Systeam_V
             else:
